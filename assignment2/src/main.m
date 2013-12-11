8f29b435bb7b3ee43844381f0cf14836c791beec
Malte Stær Nissen <malte.nissen@gmail.com>
2013-12-11 16:28:19 +0100

    Something :D

--- assignment2/src/main.m
+++ assignment2/src/main.m
@@ -6,18 +6,18 @@ run '../vlfeat-0.9.17/toolbox/vl_setup'
 
 %%
 path = '../objects/';
-trainPart = 5;
+trainPart = 10;
 testPart = 10;
-k = 1000;
+k = 2000;
 [categories, objects, train, test] = getData(path, trainPart, testPart);
-[Dtrain, DtrainCat] = getDescriptors(path, categories, objects, train, 'data/train5_10_1000Descriptors.mat');
-[Dtest, DtestCat] = getDescriptors(path, categories, objects, test, 'data/test5_10_1000Descriptors.mat');
+[Dtrain, DtrainCat] = getDescriptors(path, categories, objects, train, 'data/train10_10_2000Descriptors.mat');
+[Dtest, DtestCat] = getDescriptors(path, categories, objects, test, 'data/test10_10_2000Descriptors.mat');
 
-[C, I] = trainClusters(Dtrain, k, 'data/train5_10_1000Clusters.mat');
+[C, I] = trainClusters(Dtrain, k, 'data/train10_10_2000Clusters.mat');
 
 trainHist = bow(DtrainCat, I, k, path, categories, objects);
 
-Ci  = pushClusters(Dtest, C, 'data/test5_10_1000Clusters.mat');
+Ci  = pushClusters(Dtest, C, 'data/test10_10_2000Clusters.mat');
 
 testHist = bow(DtestCat, Ci, k, path, categories, objects);
 
